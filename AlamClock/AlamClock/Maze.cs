00fc03155226491334c1ced63af9a7c78b85063a
anagardt <anagardtasevska@gmail.com>
2018-05-28 13:46:14 +0200

    MazeGame - Scheme

---
+++ AlamClock/AlamClock/Maze.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlamClock
+{
+    public partial class Maze : Form
+    {
+        public Maze()
+        {
+            InitializeComponent();
+        }
+
+        private void Finish_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bravo!");
+            Close();
+        }
+
+        public void MoveToStart()
+        {
+            Point startingPoint = panel1.Location;
+            startingPoint.Offset(15, 15);
+            Cursor.Position = PointToScreen(startingPoint);
+        }
+
+        private void wall_MouseEnter(object sender, EventArgs e)
+        {
+            MoveToStart();
+        }
+    }
+}
